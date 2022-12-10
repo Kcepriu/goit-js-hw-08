c6824281bb56bc96ff59f583078aeb4a2e2a9c65
Kcepriu <kcepriu@gmail.com>
2022-12-10 11:19:22 +0200

    enden tast 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,39 @@
+import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
 
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Player(iframe);
+
+const KEY_LOCAL_STORAGE = 'videoplayer-current-time';
+
+// * Handler Events
+function eventTimeUpdate(event) {
+  writeToLocalStorage(event);
+}
+
+// * function works with player
+function addEventTimeUpdate() {
+  iframePlayer.on('timeupdate', throttle(eventTimeUpdate, 1000));
+}
+
+function setCurentPosition(seconds) {
+  iframePlayer.setCurrentTime(seconds);
+}
+
+// * function works with Local Storage
+function readFromLocalStorage() {
+  const seconds = localStorage.getItem('KEY_LOCAL_STORAGE');
+  return !seconds ? 0 : Number(seconds);
+}
+
+function writeToLocalStorage({ seconds } = {}) {
+  localStorage.setItem('KEY_LOCAL_STORAGE', seconds);
+}
+
+function initializeScript() {
+  setCurentPosition(readFromLocalStorage());
+
+  addEventTimeUpdate();
+}
+
+initializeScript();
